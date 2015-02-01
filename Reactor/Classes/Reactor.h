b6165cad4f5e7f5a21c9cfb8887fa728ce6ddb35
Mark Adams <mark@thoughtbot.com>
2015-02-01 00:13:45 -0800

    Add global header

---
+++ Reactor/Classes/Reactor.h
@@ -0,0 +1,13 @@
+//
+//  Reactor.h
+//  Reactor
+//
+//  Created by Mark Adams on 2/1/15.
+//  Copyright (c) 2015 Mark Adams. All rights reserved.
+//
+
+#ifndef Reactor_Reactor_h
+#define Reactor_Reactor_h
+
+
+#endif
